a1a4647c7937c8c03fc7ea4a1756f48bec553ec7
marktidalgo <mtidalgo@udel.edu>
2018-04-05 10:38:12 -0400

    added js examples

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,9 @@
+let blist = ["Sky diving", "Travel more", "Live somewhere warm"];
+console.log(blist.length);
+blist.push("Harry Potter World");
+console.log(blist.length);
+console.log(blist);
+
+for (let i=0; i<blist.length; i++){
+  console.log(blist[i]);
+}
